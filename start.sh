357a7e70f214d1fba3cb17d8b1ecce70dd3bf71b
Dmitriy Erokhin <nefariusmag@gmail.com>
2017-12-25 15:41:02 +0300

    Add new utils

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 yum -y install python2 python2-dnf
 pip3 install ansible
 ansible-playbook workspace.yml
-ansible-galaxy install viasite-ansible.zsh --force
-curl https://raw.githubusercontent.com/viasite-ansible/ansible-role-zsh/master/playbook.yml > /tmp/zsh.yml
-ansible-playbook -i "localhost," -c local /tmp/zsh.yml
-ansible-playbook -i "localhost," -c local /tmp/zsh.yml --extra-vars="zsh_user=$(whoami)"
-chsh -s /bin/zsh
+# ansible-galaxy install viasite-ansible.zsh --force
+# curl https://raw.githubusercontent.com/viasite-ansible/ansible-role-zsh/master/playbook.yml > /tmp/zsh.yml
+# ansible-playbook -i "localhost," -c local /tmp/zsh.yml
+# ansible-playbook -i "localhost," -c local /tmp/zsh.yml --extra-vars="zsh_user=$(whoami)"
+# chsh -s /bin/zsh
