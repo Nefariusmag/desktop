312e901a26cc6e73a4e4b247f3887c52fdf60d85
Dmitriy Erokhin <nefariusmag@gmail.com>
2018-02-06 11:49:47 +0300

    Hide of debian

--- start.sh
+++ start.sh
@@ -1,4 +1,13 @@
-yum -y install ansible
+if [ -n "$(cat /etc/issue | grep Debian)" ]; then
+  echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" >> /etc/apt/sources.list
+  apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
+  apt-get update
+  apt-get install ansible -y --allow-unauthenticated
+fi
+if [ -n "$(cat /etc/issue | grep RedHat)" ]; then
+  yum -y install ansible
+fi
+
 ansible-playbook workspace.yml
 # ansible-galaxy install viasite-ansible.zsh --force
 # curl https://raw.githubusercontent.com/viasite-ansible/ansible-role-zsh/master/playbook.yml > /tmp/zsh.yml
